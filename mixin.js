5b3250bbf9920da1d7e8d88c4944cc09aaa286d6
ColorChan丶 <colorchan@foxmail.com>
2017-11-14 11:22:59 +0800

    refactor word splice

--- mixin.js
+++ mixin.js
@@ -1,35 +1,32 @@
 /**
  * Created by Administrator on 2017/10/27
  */
-
-//  compute content length, chinese = 2, english = 1
-
-export function computeWordsLength(content, slice) {
+//            compute text length (ch-zn+2，en-us+1); return number
+export function computeWordsLength(content) {
     let count = 0;
-    if (slice > 0) {
-        let newStr = ''
-        for (let index in content) {
-            if ((content.charCodeAt(index) >= 0) && (content.charCodeAt(index) <= 255)) {
-                count ++
-            } else {
-                count += 2
-            }
-            if (count <= slice) {
-                newStr += content[index]
-            }
+    for (let index in content) {
+        if ((content.charCodeAt(index) >= 0) && (content.charCodeAt(index) <= 255)) {
+            count ++
+        } else {
+            count += 2
         }
-        return newStr
-
-    } else {
-        for (let index in content) {
-            if ((content.charCodeAt(index) >= 0) && (content.charCodeAt(index) <= 255)) {
-                count ++
-            } else {
-                count += 2
-            }
-        }
-        return count
     }
+    return count
+}
+
+// slice text by length (ch-zn+2，en-us+1); return text; 第二个参数number表示截取并返回字符串前几位; 第三个参数boolean，表示是否后加省略号
+export function sliceWords(content, slice, ellipsis) {
+    let count = 0;
+    let newStr = ''
+    for (let index in content) {
+        if(computeWordsLength(newStr) < slice) {
+            newStr += content[index]
+        }
+    }
+    if ((newStr !== content) && ellipsis) {
+        newStr += '...'
+    }
+    return newStr
 }
 
 export function addClass(el, className) {

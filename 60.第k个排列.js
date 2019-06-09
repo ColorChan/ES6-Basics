252fc65d14d21d86afbdb44dc1abe9fe09b412cc
SaikaChan丶 <colorchan@foxmail.com>
2019-06-09 16:10:50 +0800

    。

---
+++ 60.第k个排列.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=60 lang=javascript
+ *
+ * [60] 第k个排列
+ */
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+    
+};
+
---
+++ src/leetcode/leetcode.md
@@ -0,0 +1,4 @@
+# leetcode
+## 60.第k个排列 permutation-sequence(middle)
+思路: 
+[康托展开]: https://baike.baidu.com/item/%E5%BA%B7%E6%89%98%E5%B1%95%E5%BC%80/7968428?fr=aladdin '百度百科'
